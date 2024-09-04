f7922326ab45bd4776a89600d9349a22bd8aca51
cwl <3503127564@qq.com>
2024-09-04 17:58:22 +0800

    页面的优化

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 window.addEventListener('load', () => {
-    const nav = document.querySelector('nav');
-    const footer = document.querySelector('footer');
-    const ptwcwl = document.querySelector('ptw-cwl');
-    ptwcwl.style.marginTop = `${nav.offsetHeight}px`;
-    ptwcwl.style.marginBottom = `${footer.offsetHeight}px`;
+  const nav = document.querySelector('nav');
+  const footer = document.querySelector('footer');
+  const ptwcwl = document.querySelector('ptw-cwl');
+  ptwcwl.style.marginTop = `${nav.offsetHeight}px`;
+  ptwcwl.style.marginBottom = `${footer.offsetHeight}px`;
 });
 
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
