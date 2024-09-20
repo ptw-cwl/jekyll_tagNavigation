9827fa85795866060ea222689b42ea60043a5c60
cwl <3503127564@qq.com>
2024-09-20 10:37:58 +0800

    关联标签的添加

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,20 @@ var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
 })
 
-//搜索跳转
+// 搜索跳转
 function searchTag() {
-  var inputValue = document.getElementById('searchInput').value.trim();
-  if (inputValue) {
-      window.location.hash = inputValue;
+  var inputValue = document.getElementById('searchInput').value;
+  jump(inputValue);
+}
+
+// 跳转路径
+function jump(value) {
+  if (value) {
+    console.log(window.location);
+    console.log(window.scroll);
+    window.location.hash = value.trim();
+    console.log(window.location);
+    console.log(window.scroll);
   }
 }
+
